94409821b77cde06e9b1d552d228d2471358da5c
VaibhaviChavda <vaibhavibchavda@gmail.com>
2026-02-28 21:15:08 +0530

    Update general.js

--- general.js
+++ general.js
@@ -2,43 +2,43 @@ const axios = require("axios");
 
 const BASE_URL = "http://localhost:3000";
 
-// Task 10 - Get all books using async callback function
+// Get all books
 async function getAllBooks() {
     try {
         const response = await axios.get(`${BASE_URL}/books`);
         return response.data;
-    } catch (err) {
-        return err.message;
+    } catch (error) {
+        return error.message;
     }
 }
 
-// Task 11 - Get book by ISBN
+// Get book by ISBN
 async function getBookByISBN(isbn) {
     try {
         const response = await axios.get(`${BASE_URL}/isbn/${isbn}`);
         return response.data;
-    } catch (err) {
-        return err.message;
+    } catch (error) {
+        return error.message;
     }
 }
 
-// Task 12 - Get books by Author
+// Get books by Author
 async function getBooksByAuthor(author) {
     try {
         const response = await axios.get(`${BASE_URL}/author/${author}`);
         return response.data;
-    } catch (err) {
-        return err.message;
+    } catch (error) {
+        return error.message;
     }
 }
 
-// Task 13 - Get books by Title
+// Get books by Title
 async function getBooksByTitle(title) {
     try {
         const response = await axios.get(`${BASE_URL}/title/${title}`);
         return response.data;
-    } catch (err) {
-        return err.message;
+    } catch (error) {
+        return error.message;
     }
 }
 
